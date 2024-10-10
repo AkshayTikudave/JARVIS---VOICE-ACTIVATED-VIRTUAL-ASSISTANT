fc31d2cd97f824109ccdb0bf610fb70053448dfd
AkshayTikudave <152793779+AkshayTikudave@users.noreply.github.com>
2024-10-10 18:25:52 +0530

    Delete musiclibrary.py

--- musiclibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "millionaire" : "https://youtu.be/XO8wew38VM8?si=iYyBYgUcioAaYA7s",
-    "kk" : "https://youtu.be/VpOd36i0xII?si=jTyFPgL-9OhdrZLY",
-    "arijit" : "https://youtu.be/O5gwxm3NxFU?si=C9pp84YA0vDLxmzX",
-    "shreya" : "https://youtu.be/vjh0MtRWlKE?si=NuvnsSax-Xt-mW-s"
-}
